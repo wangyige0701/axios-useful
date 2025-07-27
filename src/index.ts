2f5f1169b4d361e829fa9dec0241cf910be7d808
WangYige <1776686515@qq.com>
2025-07-27 12:51:04 +0800

    api 优化

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
-import { checkFrequency, Fn, isDef, ParallelTask, toArray } from '@wang-yige/utils';
+import { checkFrequency, Fn, isString, ParallelTask, toArray } from '@wang-yige/utils';
 import axios, {
 	type Axios,
 	type InternalAxiosRequestConfig,
 	type AxiosResponse,
-	type AxiosRequestHeaders,
 	type AxiosInterceptorManager,
 	type CreateAxiosDefaults,
 } from 'axios';
@@ -12,23 +11,44 @@ import type {
 	InterceptRequestConfig,
 	InterceptResponseConfig,
 	RequestConfigWithAbort,
-	AbortPromise,
+	RequestPromise,
 	InitialConfig,
-} from './config';
-import { CacheController, ResponseCache } from './utils/cache';
-import { SingleController, SingleType } from './utils/single';
+} from '@/@types';
+import { CacheController } from './utils/cache';
+import { SingleController } from './utils/single';
+import { SingleType } from './utils/enum';
 
-export class APIRequest {
+const noDataMethods = ['get', 'delete', 'head', 'options'] as const;
+const withDataMethods = ['post', 'put', 'patch', 'postForm', 'putForm', 'patchForm'] as const;
+const defineConfig = {
+	writable: false,
+	configurable: false,
+};
+
+class APIRequestInstance {
 	/** The data of single types */
 	static Single = SingleType;
 
-	#userAgent?: string = void 0;
+	static create(config?: InitialConfig & CreateAxiosDefaults): APIRequestInstance;
+	static create(baseURL?: string, config?: InitialConfig & Omit<CreateAxiosDefaults, 'baseURL'>): APIRequestInstance;
+	static create(
+		baseURL?: string | (InitialConfig & CreateAxiosDefaults),
+		config?: InitialConfig & Omit<CreateAxiosDefaults, 'baseURL'>,
+	) {
+		if (isString(baseURL)) {
+			return new APIRequestInstance(baseURL, config);
+		}
+		return new APIRequestInstance(baseURL);
+	}
+
 	#domains?: string[] = void 0;
 	#maximum: number = 5;
 
 	#axios: Axios;
 	#pipeline: ParallelTask;
 	#frequency: Fn<[number, string?], any> | undefined = void 0;
+
+	// 拦截器重置
 	#requestInterceptor: {
 		onFulfilled: (
 			value: InternalAxiosRequestConfig<any>,
@@ -37,50 +57,54 @@ export class APIRequest {
 	};
 	#requestInterceptorIndex: number;
 	#responseInterceptorIndex: number;
-	// ===== controller =====
+
+	// 控制器实例
 	#cacheController: CacheController;
 	#singleController: SingleController;
-	// ======================
 
-	constructor(baseURL?: string, config?: InitialConfig & Omit<CreateAxiosDefaults, 'baseURL'>) {
-		const { userAgent, maximum = 5, domains, triggerLimit = 50 } = config || {};
-		this.#userAgent = isDef(userAgent) ? String(userAgent) : void 0;
+	constructor(config?: InitialConfig & CreateAxiosDefaults);
+	constructor(baseURL?: string, config?: InitialConfig & Omit<CreateAxiosDefaults, 'baseURL'>);
+	constructor(
+		baseURL?: string | (InitialConfig & CreateAxiosDefaults),
+		config?: InitialConfig & Omit<CreateAxiosDefaults, 'baseURL'>,
+	) {
+		if (!isString(baseURL)) {
+			config = baseURL;
+			baseURL = baseURL?.baseURL;
+		}
+		const { maximum = 5, domains, requestLimit = 50 } = config || {};
 		this.#maximum = Math.max(1, +maximum || 5);
 		this.#pipeline = new ParallelTask(this.#maximum);
-		const _limit = +triggerLimit || 0;
+		const _limit = +requestLimit || 0;
 		if (_limit > 0) {
 			this.#frequency = checkFrequency({ range: 1000, maximum: _limit }, (_, current, path) => {
 				throw new Error(
 					`The request frequency is over the limit in one second. Current count is ${current} with path '${path}' \n` +
-						"It's maybe an infinite loop, and if you want to continue, you can set the `triggerLimit` to a bigger number or zero.",
+						"It's maybe an infinite loop, and if you want to continue, you can set the `requestLimit` to a bigger number or zero.",
 				);
 			});
 		}
 		if (domains) {
 			this.#domains = toArray(domains);
 		}
-
 		const defaultConfig = { ...config };
-		delete defaultConfig.userAgent;
 		delete defaultConfig.domains;
 		delete defaultConfig.maximum;
+
 		this.#axios = axios.create({ ...defaultConfig, baseURL });
 		this.#cacheController = new CacheController(this.#axios);
 		this.#singleController = new SingleController(this.#axios, this.#pipeline, this);
 
-		this.#requestInterceptor = {
+		// 保证请求拦截器的执行顺序
+		this.#requestInterceptor = Object.freeze({
 			onFulfilled: (config: InterceptRequestConfig) => {
-				const { headers = <AxiosRequestHeaders>{} } = config;
 				this.#cacheController.request(config);
-				if (this.#userAgent && headers) {
-					headers['User-Agent'] = this.#userAgent;
-				}
 				return config;
 			},
 			onRejected: err => {
 				return Promise.reject(err);
 			},
-		};
+		});
 		this.#requestInterceptorIndex = this.#axios.interceptors.request.use(
 			this.#requestInterceptor.onFulfilled,
 			this.#requestInterceptor.onRejected,
@@ -91,12 +115,38 @@ export class APIRequest {
 				return response;
 			},
 			async err => {
-				if (err instanceof ResponseCache) {
+				if (CacheController.isResponseCache(err)) {
+					// 缓存命中
 					return Promise.resolve(err.response);
 				}
 				return Promise.reject(err);
 			},
 		);
+
+		noDataMethods.forEach(method => {
+			const _method = this.#axios[method].bind(this.#axios) as Fn<
+				[url: string, config: RequestConfig],
+				Promise<any>
+			>;
+			Object.defineProperty(this, method, {
+				...defineConfig,
+				value: <T = any, R = AxiosResponse<T>, D = any>(url: string, config?: RequestConfig<D>) => {
+					return this.#proxy<R, [string]>(_method, [url], url, config);
+				},
+			});
+		});
+		withDataMethods.forEach(method => {
+			const _method = this.#axios[method].bind(this.#axios) as Fn<
+				[url: string, data: any, config: RequestConfig],
+				Promise<any>
+			>;
+			Object.defineProperty(this, method, {
+				...defineConfig,
+				value: <T = any, R = AxiosResponse<T>, D = any>(url: string, data?: D, config?: RequestConfig<D>) => {
+					return this.#proxy<R, [string, D?]>(_method, [url, data], url, config);
+				},
+			});
+		});
 	}
 
 	/**
@@ -106,85 +156,134 @@ export class APIRequest {
 		return this.#domains;
 	}
 
+	get defaults() {
+		return this.#axios.defaults;
+	}
+
+	#interceptors: Axios['interceptors'] | undefined;
+
 	/**
-	 * The interceptor for request
+	 * Request interceptor
 	 */
-	get request() {
+	get interceptors() {
+		if (this.#interceptors) {
+			return this.#interceptors;
+		}
+		this.#interceptors = Object.create(null);
 		const request = this.#axios.interceptors.request;
-		return {
-			use: (...args) => {
-				request.eject(this.#requestInterceptorIndex);
-				const index = request.use(...args);
-				this.#requestInterceptorIndex = request.use(
-					this.#requestInterceptor.onFulfilled,
-					this.#requestInterceptor.onRejected,
-				);
-				return index;
-			},
-			eject: (id: number) => {
-				if (id === this.#requestInterceptorIndex) {
-					return;
-				}
-				return request.eject(id);
-			},
-			clear: () => {
-				return request.clear();
-			},
-		} as AxiosInterceptorManager<InternalAxiosRequestConfig<any>>;
-	}
-
-	/**
-	 * The interceptor for response
-	 */
-	get response() {
 		const response = this.#axios.interceptors.response;
-		return {
-			use: (...args) => {
-				return response.use(...args);
-			},
-			eject: (id: number) => {
-				if (id === this.#responseInterceptorIndex) {
-					return;
-				}
-				return response.eject(id);
-			},
-			clear: () => {
-				return response.clear();
-			},
-		} as AxiosInterceptorManager<AxiosResponse<any, any>>;
-	}
-
-	get<T = any, R = AxiosResponse<T>, D = any>(url: string, config?: RequestConfig<D>) {
-		return this.#proxy<R>(this.#axios.get.bind(this.#axios, url), url, config);
-	}
-
-	post<T = any, R = AxiosResponse<T>, D = any>(url: string, data?: D, config?: RequestConfig<D>) {
-		return this.#proxy<R>(this.#axios.post.bind(this, url, data), url, config);
-	}
-
-	put<T = any, R = AxiosResponse<T>, D = any>(url: string, data?: D, config?: RequestConfig<D>) {
-		return this.#proxy<R>(this.#axios.put.bind(this, url, data), url, config);
-	}
-
-	delete<T = any, R = AxiosResponse<T>, D = any>(url: string, config?: RequestConfig<D>) {
-		return this.#proxy<R>(this.#axios.delete.bind(this, url), url, config);
-	}
-
-	userAgent(str: string) {
-		this.#userAgent = str;
+		Object.defineProperty(this.#interceptors, 'request', {
+			...defineConfig,
+			value: Object.freeze({
+				use: (...args) => {
+					request.eject(this.#requestInterceptorIndex);
+					const index = request.use(...args);
+					this.#requestInterceptorIndex = request.use(
+						this.#requestInterceptor.onFulfilled,
+						this.#requestInterceptor.onRejected,
+					);
+					return index;
+				},
+				eject: (id: number) => {
+					if (id === this.#requestInterceptorIndex) {
+						return;
+					}
+					return request.eject(id);
+				},
+				clear: () => request.clear(),
+			} as AxiosInterceptorManager<InternalAxiosRequestConfig<any>>),
+		});
+		Object.defineProperty(this.#interceptors, 'response', {
+			...defineConfig,
+			value: Object.freeze({
+				use: (...args) => response.use(...args),
+				eject: (id: number) => {
+					if (id === this.#responseInterceptorIndex) {
+						return;
+					}
+					return response.eject(id);
+				},
+				clear: () => response.clear(),
+			} as AxiosInterceptorManager<AxiosResponse<any, any>>),
+		});
+		return this.#interceptors!;
 	}
 
 	/**
 	 * Change the maximum number of parallel requests pipeline.
 	 */
-	pipelineMaximum(maximum: number) {
+	maximum(maximum: number) {
 		this.#pipeline.changeMaxParallelCount(Math.max(1, +maximum || 5));
 	}
 
-	#proxy<R>(fn: Fn<[config: RequestConfig], Promise<any>>, url: string, config: RequestConfigWithAbort = {}) {
-		if (this.#frequency) {
-			this.#frequency(1, url);
-		}
-		return this.#singleController.request<R>(fn, url, { ...config }) as AbortPromise<R>;
+	getUri(config?: RequestConfig) {
+		return this.#axios.getUri(config);
 	}
+
+	// api methods
+
+	#proxy<R, P extends any[]>(
+		fn: Fn<[...P, config: RequestConfig], Promise<any>>,
+		rests: P,
+		url: string,
+		config: RequestConfigWithAbort = {},
+	) {
+		this.#frequency && this.#frequency(1, url);
+		return this.#singleController.request<R, P>(fn, rests, url, { ...config }) as RequestPromise<R>;
+	}
+
+	// @ts-expect-error
+	get<T = any, R = AxiosResponse<T>, D = any>(url: string, config?: RequestConfig<D>): RequestPromise<R>;
+	// @ts-expect-error
+	delete<T = any, R = AxiosResponse<T>, D = any>(url: string, config?: RequestConfig<D>): RequestPromise<R>;
+	// @ts-expect-error
+	head<T = any, R = AxiosResponse<T>, D = any>(url: string, config?: RequestConfig<D>): RequestPromise<R>;
+	// @ts-expect-error
+	options<T = any, R = AxiosResponse<T>, D = any>(url: string, config?: RequestConfig<D>): RequestPromise<R>;
+	// @ts-expect-error
+	post<T = any, R = AxiosResponse<T>, D = any>(url: string, data?: D, config?: RequestConfig<D>): RequestPromise<R>;
+	// @ts-expect-error
+	put<T = any, R = AxiosResponse<T>, D = any>(url: string, data?: D, config?: RequestConfig<D>): RequestPromise<R>;
+	// @ts-expect-error
+	patch<T = any, R = AxiosResponse<T>, D = any>(url: string, data?: D, config?: RequestConfig<D>): RequestPromise<R>;
+	// @ts-expect-error
+	// prettier-ignore
+	postForm<T = any, R = AxiosResponse<T>, D = any>(url: string, data?: D, config?: RequestConfig<D>): RequestPromise<R>
+	// @ts-expect-error
+	// prettier-ignore
+	putForm<T = any, R = AxiosResponse<T>, D = any>(url: string, data?: D, config?: RequestConfig<D>): RequestPromise<R>
+	// @ts-expect-error
+	// prettier-ignore
+	patchForm<T = any, R = AxiosResponse<T>, D = any>(url: string, data?: D, config?: RequestConfig<D>): RequestPromise<R>
 }
+
+let defaultInstance: APIRequestInstance;
+noDataMethods.forEach(method => {
+	Object.defineProperty(APIRequestInstance, method, {
+		...defineConfig,
+		value: (url: string, config: RequestConfigWithAbort = {}) => {
+			if (!defaultInstance) {
+				defaultInstance = new APIRequestInstance('');
+			}
+			return defaultInstance[method](url, config);
+		},
+	});
+});
+withDataMethods.forEach(method => {
+	Object.defineProperty(APIRequestInstance, method, {
+		...defineConfig,
+		value: (url: string, data: any, config: RequestConfigWithAbort = {}) => {
+			if (!defaultInstance) {
+				defaultInstance = new APIRequestInstance('');
+			}
+			return defaultInstance[method](url, data, config);
+		},
+	});
+});
+
+type APIRequestStatic = typeof APIRequestInstance &
+	Pick<APIRequestInstance, (typeof noDataMethods)[number] | (typeof withDataMethods)[number]>;
+
+const APIRequest: APIRequestStatic = APIRequestInstance as any;
+
+export { APIRequest, axios };
